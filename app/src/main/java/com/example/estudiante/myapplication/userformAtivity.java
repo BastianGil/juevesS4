a305728dbc4788516c868234a0f21c7bd5e15848
BastianGil <8sebasgil@gmail.com>
2018-08-23 13:01:50 -0500

    Añadir variable a constans

--- app/src/main/java/com/example/estudiante/myapplication/userformAtivity.java
+++ app/src/main/java/com/example/estudiante/myapplication/userformAtivity.java
@@ -3,6 +3,7 @@ package com.example.estudiante.myapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -29,6 +30,26 @@ public class userformAtivity extends AppCompatActivity {
         getEdtUserFormUsername= findViewById(R.id.edt_userform_username);
 
         btn_UserForm_Finish = findViewById(R.id.btn_userform_finish);
+
+        btn_UserForm_Finish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent respuesta = new Intent();
+
+                String name = edtUserFormName.getText().toString();
+                String email = getEdtUserFormEmail.getText().toString();
+                String username = getEdtUserFormUsername.getText().toString();
+
+                respuesta.putExtra( "name", name);
+                respuesta.putExtra( "email", email);
+                respuesta.putExtra( "uesrname", username);
+
+              setResult(Constans.RESP_NEW_USER, respuesta);
+              finish();
+
+            }
+        });
+
 
     }
 }
